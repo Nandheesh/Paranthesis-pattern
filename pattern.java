288ede11583d71ad0277ce5f4a76410da9399adb
Nandheeshwar G <60293215+Nandheesh@users.noreply.github.com>
2022-12-29 20:57:44 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,4 +1,4 @@
-5
+Input 1 : 5
 ****()****
 ***(())***
 **((()))**
@@ -9,7 +9,25 @@
 ***(())***
 ****()****
 
-2
+Input 2 : 2
 *()*
 (())
 *()*
+
+
+mport java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+		int n = new Scanner(System.in).nextInt(), st = n-1;
+		while(st>=0){
+		    System.out.println("*".repeat(st)+"(".repeat(n-st)+")".repeat(n-st)+"*".repeat(st));
+		    st--;
+		}
+		st = 1;
+		while(st<n){
+		    System.out.println("*".repeat(st)+"(".repeat(n-st)+")".repeat(n-st)+"*".repeat(st));
+		    st++;
+		}
+    }
+}
